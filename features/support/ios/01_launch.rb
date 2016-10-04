aeca95bb3e2dca160e26d9900204588946354c37
Dan Buckland <danbucklan@gmail.com>
2016-10-04 15:34:22 +0100

    Update iOS launch and config files for iOS 10 and add auto generated files

--- features/support/ios/01_launch.rb
+++ features/support/ios/01_launch.rb
@@ -1,21 +1,22 @@
-########################################
-#                                      #
-#       Important Note                 #
-#                                      #
-#   When running calabash-ios tests at #
-#   www.xamarin.com/test-cloud         #
-#   this file will be overwritten by   #
-#   a file which automates             #
-#   app launch on devices.             #
-#                                      #
-#   Don't rely on this file being      #
-#   present when running at            #
-#   Xamarin Test Cloud                 #
-#                                      #
-########################################
-
 require 'calabash-cucumber/launcher'
 
+# You can find examples of more complicated launch hooks in these
+# two repositories:
+#
+# https://github.com/calabash/ios-smoke-test-app/blob/master/CalSmokeApp/features/support/01_launch.rb
+# https://github.com/calabash/ios-webview-test-app/blob/master/CalWebViewApp/features/support/01_launch.rb
+
+module Calabash::Launcher
+  @@launcher = nil
+
+  def self.launcher
+    @@launcher ||= Calabash::Cucumber::Launcher.new
+  end
+
+  def self.launcher=(launcher)
+    @@launcher = launcher
+  end
+end
 
 Before do |scenario|
 
@@ -32,22 +33,28 @@ Before do |scenario|
   @app      ||= page(App)
 	@screen   ||= page(Screens)
 
-  @calabash_launcher = Calabash::Cucumber::Launcher.new
   scenario_tags = scenario.source_tag_names
-  if scenario_tags.include?('@reinstall')
-    @calabash_launcher.reset_app_jail
-  end
-  unless @calabash_launcher.calabash_no_launch?
-    @calabash_launcher.relaunch()
-    @calabash_launcher.calabash_notify(self)
-  end
+  condition = (scenario_tags.include?('@reinstall') || scenario_tags.include?('@reset')) ? true : false
+
+  launcher = Calabash::Launcher.launcher
+  options = {
+    reset: condition
+  }
+
+  launcher.relaunch(options)
+  launcher.calabash_notify(self)
 end
 
 After do |scenario|
-  unless @calabash_launcher.calabash_no_stop?
+  # Calabash can shutdown the app cleanly by calling the app life cycle methods
+  # in the UIApplicationDelegate.  This is really nice for CI environments, but
+  # not so good for local development.
+  #
+  # See the documentation for QUIT_APP_AFTER_SCENARIO for a nice debugging workflow
+  #
+  # http://calabashapi.xamarin.com/ios/file.ENVIRONMENT_VARIABLES.html#label-QUIT_APP_AFTER_SCENARIO
+  # http://calabashapi.xamarin.com/ios/Calabash/Cucumber/Core.html#console_attach-instance_method
+  if launcher.quit_app_after_scenario?
     calabash_exit
-    if @calabash_launcher.active?
-      @calabash_launcher.stop
-    end
   end
 end
